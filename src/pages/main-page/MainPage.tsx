cec876d612e99cd07e3b7fad879261d36b90630e
Yury Kinchin <yury.kinchyn@gmail.com>
2024-03-07 15:30:17 +0300

    feat: add main page content

--- src/pages/main-page/MainPage.tsx
+++ src/pages/main-page/MainPage.tsx
@@ -1,5 +1,27 @@
+import { Flex, Typography } from 'antd';
+import Advantages from './components/advantages/Advantages';
+import BottomContent from './components/bottomContent/BottomContent';
+import s from './mainPage.module.scss';
+
+const { Title, Text, Paragraph } = Typography;
+
 const MainPage = () => {
-    return <div>MainPage</div>;
+    return (
+        <>
+            <div className={s.wrapper}>
+                <Flex vertical gap={30}>
+                    <Flex vertical>
+                        <Title>Discover Events Nearby with Ease!</Title>
+                        <Text type="secondary">
+                            Never Miss Out on Exciting Opportunities Again
+                        </Text>
+                    </Flex>
+                    <Advantages />
+                </Flex>
+                <BottomContent />
+            </div>
+        </>
+    );
 };
 
 export default MainPage;
