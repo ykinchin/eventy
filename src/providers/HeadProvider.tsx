7a217456efd43fd50d2b11612b72faa43f420765
Yury Kinchin <yury.kinchyn@gmail.com>
2024-03-07 13:59:16 +0300

    feat: add main provider

--- src/providers/HeadProvider.tsx
+++ src/providers/HeadProvider.tsx
@@ -1,11 +1,14 @@
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { store } from '../store/store';
+import AntdProvider from './AntdProvider';
 
 const HeadProvider = ({ children }: { children: React.JSX.Element }) => {
     return (
         <Provider store={store}>
-            <BrowserRouter>{children}</BrowserRouter>
+            <BrowserRouter>
+                <AntdProvider>{children}</AntdProvider>
+            </BrowserRouter>
         </Provider>
     );
 };
