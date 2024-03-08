4a08e165a47880105dccaa8b4f870f99f14b284f
Yury Kinchin <yury.kinchyn@gmail.com>
2024-03-08 16:44:03 +0300

    feat: create loader component

---
+++ src/components/shared/loader/Loader.tsx
@@ -0,0 +1,24 @@
+import Lottie from 'react-lottie';
+
+import animationData from './loader.json';
+
+import styles from './loader.module.scss';
+
+const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+        preserveAspectRatio: 'xMidYMid slice',
+    },
+};
+
+const Loader = () => (
+    <div className={styles.wrapper} data-test-id="loader">
+        <div className={styles.loader}>
+            <Lottie options={defaultOptions} />;
+        </div>
+    </div>
+);
+
+export default Loader;
