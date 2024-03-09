bd08e8009583ca14cdac761b3fff60f79bb3e136
Yury Kinchin <yury.kinchyn@gmail.com>
2024-03-09 15:08:36 +0300

    feat: add new props

--- src/components/shared/customButton/CustomButton.tsx
+++ src/components/shared/customButton/CustomButton.tsx
@@ -3,10 +3,23 @@ import { Button } from 'antd';
 type Props = {
     children: React.ReactNode;
     type?: 'dashed' | 'default' | 'primary' | 'link' | 'text';
+    block?: boolean;
+    size?: 'large' | 'middle' | 'small';
+    onClick?: () => void;
 };
 
-const CustomButton = ({ children, type = 'primary' }: Props) => {
-    return <Button type={type}>{children}</Button>;
+const CustomButton = ({
+    children,
+    type = 'primary',
+    block = false,
+    size,
+    onClick,
+}: Props) => {
+    return (
+        <Button type={type} block={block} size={size} onClick={onClick}>
+            {children}
+        </Button>
+    );
 };
 
 export default CustomButton;
