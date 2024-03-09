2e4d36877a925c65c71d13fbbcee6bb3ae106517
Yury Kinchin <yury.kinchyn@gmail.com>
2024-03-09 15:10:14 +0300

    feat: create eventItem component

--- src/pages/events-page/components/eventItem/EventItem.tsx
+++ src/pages/events-page/components/eventItem/EventItem.tsx
@@ -1,5 +1,32 @@
-const EventItem = () => {
-    return <div>EventItem</div>;
+import { Card, Flex, Typography } from 'antd';
+import { useNavigate } from 'react-router-dom';
+import { IEvent } from '../../../../shared/types/eventTypes';
+import s from './eventItem.module.scss';
+
+type Props = {
+    event: IEvent;
+};
+
+const { Text } = Typography;
+
+const EventItem = ({ event }: Props) => {
+
+    return (
+        <Card
+            hoverable
+            className={s.card}
+            bordered={false}
+        >
+            <Flex justify="space-between" align="center">
+                <Flex vertical>
+                </Flex>
+                <Flex gap={60} justify="space-between">
+                    <Flex vertical>
+                    </Flex>
+                </Flex>
+            </Flex>
+        </Card>
+    );
 };
 
 export default EventItem;
