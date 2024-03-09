51e2b2f8c2ec17bb9cac0deffda71a54cc8fccd3
Yury Kinchin <yury.kinchyn@gmail.com>
2024-03-09 15:38:22 +0300

    fix: fix ui

--- src/pages/events-page/components/eventList/EventList.tsx
+++ src/pages/events-page/components/eventList/EventList.tsx
@@ -1,36 +1,19 @@
-import { Flex, Input, List, Pagination, Select } from 'antd';
-import { useState } from 'react';
-import InputSearch from '../../../../components/shared/inputSearch/InputSearch';
-import EventItem from '../eventItem/EventItem';
+import { Flex, List, Pagination, Select } from 'antd';
 import s from './eventList.module.scss';
 
 const EventList = () => {
-
     return (
-        <>
-            ) : (
-                <Flex vertical justify="space-between" className={s.wrapper}>
-                    <Flex justify="center" vertical gap={40}>
-                        <Flex gap={20}>
-                            <Select
-                                style={{ width: 130 }}
-                                ]}
-                            />
-                        </Flex>
-                        <List
-                            style={{ height: '70vh', overflow: 'auto' }}
-                        />
-                    </Flex>
-                    <Flex justify="center">
-                        <Pagination
-                            showSizeChanger={false}
-                            hideOnSinglePage={true}
-                        />
-                    </Flex>
+        <Flex vertical justify="space-between" className={s.wrapper}>
+            <Flex justify="center" vertical gap={40}>
+                <Flex gap={20}>
+                    <Select style={{ width: 130 }} />
                 </Flex>
-            )}
-        </>
+                <List style={{ height: '70vh', overflow: 'auto' }} />
+            </Flex>
+            <Flex justify="center">
+                <Pagination showSizeChanger={false} hideOnSinglePage={true} />
+            </Flex>
+        </Flex>
     );
 };
-
 export default EventList;
