2d25058bedf1626330eba87364cc5a815f9dac25
Yury Kinchin <yury.kinchyn@gmail.com>
2024-03-11 21:44:03 +0300

    feat: create auth layout

---
+++ src/components/layouts/authLayout/AuthLayout.tsx
@@ -0,0 +1,18 @@
+import { Layout } from 'antd';
+import { Content } from 'antd/es/layout/layout';
+import { Outlet } from 'react-router-dom';
+import s from './authLayout.module.scss';
+
+const AuthLayout = () => {
+    return (
+        <Layout style={{ height: '100%' }}>
+            <Content className={s.content}>
+                <div className={s.wrapper}>
+                    <Outlet />
+                </div>
+            </Content>
+        </Layout>
+    );
+};
+
+export default AuthLayout;
