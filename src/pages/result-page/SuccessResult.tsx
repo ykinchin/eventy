e4a168f3ac1b98019004e6cec5ba4fc6dbbed016
Yury Kinchin <yury.kinchyn@gmail.com>
2024-03-13 19:18:16 +0300

    feat: add protected result route

--- src/pages/result-page/SuccessResult.tsx
+++ src/pages/result-page/SuccessResult.tsx
@@ -1,7 +1,14 @@
+import { Navigate, useLocation } from 'react-router-dom';
 import ResultCard from '../../components/shared/resultCard/ResultCard';
 import { PATHS } from '../../shared/constants/paths';
 
 const SuccessResult = () => {
+    const location = useLocation();
+
+    if (location.state?.prevPath !== PATHS.registration) {
+        return <Navigate to={PATHS.auth} />;
+    }
+
     return (
         <ResultCard
             status="success"
