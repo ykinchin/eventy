5862df33a8da168ee42d011d2c6253094b776e18
Yury Kinchin <yury.kinchyn@gmail.com>
2024-03-13 19:17:41 +0300

    feat: add conditional rendering

--- src/components/layouts/mainLayout/header/MainHeader.tsx
+++ src/components/layouts/mainLayout/header/MainHeader.tsx
@@ -1,4 +1,8 @@
 import { Flex, Layout, Typography } from 'antd';
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../../../providers/AuthContext';
+import { PATHS } from '../../../../shared/constants/paths';
 import CustomButton from '../../../shared/customButton/CustomButton';
 import Logo from '../../../shared/logo/Logo';
 import NavMenu from './NavMenu';
@@ -8,16 +12,28 @@ const { Header } = Layout;
 const { Text } = Typography;
 
 const MainHeader = () => {
+    const { logOut, currentUser, isLoading } = useContext(AuthContext);
+    const navigate = useNavigate();
+    const handleOnClick = () => {
+        currentUser ? logOut() : navigate(PATHS.auth);
+    };
+
     return (
         <Header className={s.header}>
             <Flex align="center" className={s.navigation}>
-                <div className={s.logo}>
-                    <Logo />
-                </div>
-                <NavMenu />
+                <Flex>
+                    <div className={s.logo}>
+                        <Logo />
+                    </div>
+                    <NavMenu />
+                </Flex>
                 <Flex align="center" gap={22} className={s.rightMenu}>
-                    <Text className={s.userLabel}>UserName</Text>
-                    <CustomButton type="default">Logout</CustomButton>
+                    <Text className={s.userLabel}>{currentUser}</Text>
+                    {!isLoading && (
+                        <CustomButton type="default" onClick={handleOnClick}>
+                            {currentUser ? ' Logout' : 'Sign In'}
+                        </CustomButton>
+                    )}
                 </Flex>
             </Flex>
         </Header>
