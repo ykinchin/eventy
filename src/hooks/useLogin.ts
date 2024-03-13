3a87f884116693ffe323335da4cfd34736e5cb27
Yury Kinchin <yury.kinchyn@gmail.com>
2024-03-13 19:22:17 +0300

    feat: add useLogin hook

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,5 +1,34 @@
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { auth } from '../firebase/firebase';
+import { PATHS } from '../shared/constants/paths';
+import { UserCredentials } from '../shared/types/userType';
+
+const useLogin = () => {
+    const navigate = useNavigate();
+    const [isLoading, setIsLoading] = useState(false);
+
+    const loginUser = async (userForm: UserCredentials) => {
+        setIsLoading(true);
+        try {
+            await signInWithEmailAndPassword(
+                auth,
+                userForm.email,
+                userForm.password,
+            );
+            navigate(PATHS.main);
         } catch (error: unknown) {
             navigate(PATHS.errorResult, {
                 state: { prevPath: PATHS.auth },
                 replace: true,
             });
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    return { isLoading, loginUser };
+};
+
+export default useLogin;
