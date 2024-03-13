97f6d1c5b477ac4bb77ab5ac3d482092e6abb0f6
Yury Kinchin <yury.kinchyn@gmail.com>
2024-03-13 19:23:20 +0300

    feat: add redirect if user is logged in

--- src/pages/auth-page/AuthPage.tsx
+++ src/pages/auth-page/AuthPage.tsx
@@ -1,7 +1,15 @@
+import { Navigate } from 'react-router-dom';
+import Loader from '../../components/shared/loader/Loader';
+import { useAuth } from '../../hooks/useAuth';
+import { PATHS } from '../../shared/constants/paths';
 import AuthCard from './components/authCard/AuthCard';
 
 const AuthPage = () => {
-    return <AuthCard />;
+    const { currentUser, isLoading } = useAuth();
+
+    if (currentUser) return <Navigate to={PATHS.main} replace />;
+
+    return isLoading ? <Loader /> : <AuthCard />;
 };
 
 export default AuthPage;
