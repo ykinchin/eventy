237cad113096fe8eef90252d08ecae6a427df572
Yury Kinchin <yury.kinchyn@gmail.com>
2024-03-14 14:14:23 +0300

    feat: add error catch for logout

--- src/providers/AuthContext.tsx
+++ src/providers/AuthContext.tsx
@@ -1,6 +1,8 @@
 import { onAuthStateChanged, signOut } from 'firebase/auth';
 import { ReactNode, createContext, useEffect, useMemo, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { auth } from '../firebase/firebase';
+import { PATHS } from '../shared/constants/paths';
 
 type Props = {
     children?: ReactNode;
@@ -19,6 +21,7 @@ export const AuthContext = createContext<ContextType>({
 });
 
 export const AuthProvider = ({ children }: Props) => {
+    const navigate = useNavigate();
     const [currentUser, setCurrentUser] = useState<string | null>(null);
     const [isLoading, setIsLoading] = useState(true);
 
@@ -39,8 +42,15 @@ export const AuthProvider = ({ children }: Props) => {
     }, []);
 
     const logOut = async () => {
-        await signOut(auth);
-        setCurrentUser(null);
+        try {
+            setIsLoading(true);
+            await signOut(auth);
+            setCurrentUser(null);
+        } catch {
+            navigate(PATHS.errorDefault, { state: { prevPath: PATHS.auth } });
+        } finally {
+            setIsLoading(false);
+        }
     };
 
     const value = useMemo(
