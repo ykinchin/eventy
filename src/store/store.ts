9a29d0c6586918fd9ec750d9cc4f7a259ba536bc
Yury Kinchin <yury.kinchyn@gmail.com>
2024-03-15 18:55:13 +0300

    feat: add favoriteApi to store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,16 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { eventApi } from './eventsSlice/eventApi';
+import { favoriteApi } from './favoriteSlice/favoriteApi';
 
 export const store = configureStore({
     reducer: combineReducers({
         [eventApi.reducerPath]: eventApi.reducer,
+        [favoriteApi.reducerPath]: favoriteApi.reducer,
     }),
     middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(eventApi.middleware),
+        getDefaultMiddleware()
+            .concat(eventApi.middleware)
+            .concat(favoriteApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
