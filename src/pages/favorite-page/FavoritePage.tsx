afa2c56e0f18d44f2de8fb8c674e68410ae1cb49
Yury Kinchin <yury.kinchyn@gmail.com>
2024-03-17 13:45:16 +0300

    feat: add new favorite page

--- src/pages/favorite-page/FavoritePage.tsx
+++ src/pages/favorite-page/FavoritePage.tsx
@@ -1,5 +1,56 @@
+import { useState } from 'react';
+import EventList from '../../components/shared/eventList/EventList';
+import { useAuth } from '../../hooks/useAuth';
+import useDebounce from '../../hooks/useDebounce';
+import { Sort } from '../../shared/types/eventTypes';
+import { useGetEventsQuery } from '../../store/eventsSlice/eventApi';
+import { useFetchFavoriteQuery } from '../../store/favoriteSlice/favoriteApi';
+import EmptyPage from './components/emptyPage/EmptyPage';
+
 const FavoritePage = () => {
-    return <div>FavoritePage</div>;
+    const { currentUser } = useAuth();
+    const [searchValue, setSearchValue] = useState<null | string>(null);
+    const [cityValue, setCityValue] = useState<null | string>(null);
+    const [sortValue, setSortValue] = useState<Sort>('relevance,desc');
+    const debouncedSearch = useDebounce(searchValue, 300);
+    const debouncedCity = useDebounce(cityValue, 300);
+
+    const { data: favoriteData } = useFetchFavoriteQuery(currentUser);
+    const favoriteIds = favoriteData && favoriteData.map((fav) => fav.eventId);
+
+    const { data: { events } = {}, isLoading } = useGetEventsQuery({
+        eventIds: favoriteIds,
+        searchValue: debouncedSearch,
+        city: debouncedCity,
+        sort: sortValue,
+    });
+
+    const handleSort = (value: Sort) => {
+        setSortValue(value);
+    };
+
+    const handleCitySearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setCityValue(e.currentTarget.value);
+    };
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchValue(e.currentTarget.value);
+    };
+    return (
+        <div style={{ height: '100%' }}>
+            {favoriteIds?.length === 0 ? (
+                <EmptyPage />
+            ) : (
+                <EventList
+                    events={events}
+                    isLoading={isLoading}
+                    handleInputChange={handleInputChange}
+                    handleSort={handleSort}
+                    handleCitySearch={handleCitySearch}
+                />
+            )}
+        </div>
+    );
 };
 
 export default FavoritePage;
