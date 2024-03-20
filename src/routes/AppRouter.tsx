0ac55a7ce3fdf679ccc353f8c26ee6874ac189fa
Yury Kinchin <yury.kinchyn@gmail.com>
2024-03-20 19:51:34 +0300

    feat: replace routes with lazy components

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,44 +1,70 @@
+import { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import AppLayout from '../components/layouts/AppLayout';
-import AuthLayout from '../components/layouts/authLayout/AuthLayout';
-import MainLayout from '../components/layouts/mainLayout/MainLayout';
-import AuthPage from '../pages/auth-page/AuthPage';
-import EventsPage from '../pages/events-page/EventsPage';
-import FavoritePage from '../pages/favorite-page/FavoritePage';
-import MainPage from '../pages/main-page/MainPage';
-import ErrorDefault from '../pages/result-page/ErrorDefault';
-import ErrorResult from '../pages/result-page/ErrorResult';
-import ErrorUserExistResult from '../pages/result-page/ErrorUserExistResult';
-import SuccessResult from '../pages/result-page/SuccessResult';
+import Loader from '../components/shared/loader/Loader';
 import { PATHS } from '../shared/constants/paths';
+import { lazyLoadedComponents } from './lazyRoutes';
+
+const {
+    AppLayout,
+    AuthLayout,
+    MainLayout,
+    AuthPage,
+    EventsPage,
+    FavoritePage,
+    HistoryPage,
+    MainPage,
+    ErrorDefault,
+    ErrorResult,
+    ErrorUserExistResult,
+    SuccessResult,
+    SearchPage,
+    SingleEventPage,
+} = lazyLoadedComponents;
 
 const AppRouter = () => {
     return (
-        <Routes>
-            <Route path={PATHS.main} element={<AppLayout />}>
-                <Route element={<MainLayout />}>
-                    <Route index element={<MainPage />} />
-                    <Route path={PATHS.events} element={<EventsPage />} />
-                    <Route path={PATHS.favorite} element={<FavoritePage />} />
+        <Suspense fallback={<Loader />}>
+            <Routes>
+                <Route path={PATHS.main} element={<AppLayout />}>
+                    <Route element={<MainLayout />}>
+                        <Route index element={<MainPage />} />
+                        <Route path={PATHS.events} element={<EventsPage />} />
+                        <Route
+                            path="/events/search/"
+                            element={<SearchPage />}
+                        />
+                        <Route
+                            path={PATHS.eventPage}
+                            element={<SingleEventPage />}
+                        />
+                        <Route path={PATHS.history} element={<HistoryPage />} />
+                        <Route
+                            path={PATHS.favorite}
+                            element={<FavoritePage />}
+                        />
+                    </Route>
+                    <Route path={PATHS.auth} element={<AuthLayout />}>
+                        <Route index element={<AuthPage />} />
+                        <Route
+                            path={PATHS.successResult}
+                            element={<SuccessResult />}
+                        />
+                        <Route
+                            path={PATHS.errorResult}
+                            element={<ErrorResult />}
+                        />
+                        <Route
+                            path={PATHS.errorDefault}
+                            element={<ErrorDefault />}
+                        />
+                        <Route
+                            path={PATHS.errorUserExistResult}
+                            element={<ErrorUserExistResult />}
+                        />
+                    </Route>
                 </Route>
-                <Route path={PATHS.auth} element={<AuthLayout />}>
-                    <Route index element={<AuthPage />} />
-                    <Route
-                        path={PATHS.successResult}
-                        element={<SuccessResult />}
-                    />
-                    <Route path={PATHS.errorResult} element={<ErrorResult />} />
-                    <Route
-                        path={PATHS.errorDefault}
-                        element={<ErrorDefault />}
-                    />
-                    <Route
-                        path={PATHS.errorUserExistResult}
-                        element={<ErrorUserExistResult />}
-                    />
-                </Route>
-            </Route>
-        </Routes>
+            </Routes>
+        </Suspense>
     );
 };
 
