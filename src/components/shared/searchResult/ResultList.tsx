75a5e440350af6830d9e52e70a40f63c8277f280
Yury Kinchin <yury.kinchyn@gmail.com>
2024-03-20 19:53:23 +0300

    feat: create result list component

---
+++ src/components/shared/searchResult/ResultList.tsx
@@ -0,0 +1,41 @@
+import { List, Typography } from 'antd';
+import { useNavigate } from 'react-router-dom';
+import { FilteredEvent } from '../../../shared/types/eventTypes';
+import s from './resultList.module.scss';
+
+type Props = { events?: FilteredEvent[] };
+
+const { Text } = Typography;
+
+const ResultList = ({ events }: Props) => {
+    const navigate = useNavigate();
+    const handleOnClick = (id: string) => {
+        navigate(`/${id}`);
+    };
+
+    return (
+        <List
+            style={{
+                position: 'absolute',
+                top: 35,
+                width: '100%',
+            }}
+            dataSource={events}
+            className={s.list}
+            renderItem={(item) => (
+                <List.Item
+                    key={item.id}
+                    className={s.item}
+                    onClick={() => handleOnClick(item.id)}
+                >
+                    <Text ellipsis className={s.itemTitle}>
+                        {item.name}
+                    </Text>
+                    <Text>{item.date}</Text>
+                </List.Item>
+            )}
+        />
+    );
+};
+
+export default ResultList;
