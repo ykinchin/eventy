532b235dd6b32bac82dae738f5262881e34839bf
Yury Kinchin <yury.kinchyn@gmail.com>
2024-03-20 19:54:26 +0300

    feat: create historyList component

---
+++ src/pages/history-page/components/historyList/HistoryList.tsx
@@ -0,0 +1,31 @@
+import { List } from 'antd';
+import { useAuth } from '../../../../hooks/useAuth';
+import { HistoryType } from '../../../../shared/types/historyTypes';
+import { useRemoveHistoryMutation } from '../../../../store/historySlice/historyApi';
+import HistoryItem from '../historyItem/HistoryItem';
+
+type Props = {
+    history?: HistoryType[];
+};
+
+const HistoryList = ({ history }: Props) => {
+    const { currentUser } = useAuth();
+    const [removeHistory] = useRemoveHistoryMutation();
+    const handleOnRemove = (id: string) => {
+        removeHistory({ email: currentUser, historyId: id, history });
+    };
+
+    return (
+        <List
+            style={{ height: '70vh', overflow: 'auto' }}
+            dataSource={history}
+            renderItem={(item) => (
+                <List.Item>
+                    <HistoryItem historyItem={item} onRemove={handleOnRemove} />
+                </List.Item>
+            )}
+        />
+    );
+};
+
+export default HistoryList;
