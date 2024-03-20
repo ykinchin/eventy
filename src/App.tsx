7cb9231f37c22fe2d3869e05bad370573e3f2fb5
Yury Kinchin <yury.kinchyn@gmail.com>
2024-03-20 19:57:57 +0300

    feat: wrap app with errorBoundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import './assets/styles/_variablex.scss';
 import './assets/styles/index.css';
+import AppErrorBoundary from './components/errorBoundary/AppErrorBoundary';
 import AppRouter from './routes/AppRouter';
 
 function App() {
-    return <AppRouter />;
+    return (
+        <AppErrorBoundary>
+            <AppRouter />
+        </AppErrorBoundary>
+    );
 }
 
 export default App;
