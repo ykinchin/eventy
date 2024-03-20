57ff67e8feffefdd9ee25f16050194e5b7ce999a
Yury Kinchin <yury.kinchyn@gmail.com>
2024-03-20 20:03:23 +0300

    feat: create inputSearch component

--- src/components/shared/inputSearch/InputSearch.tsx
+++ src/components/shared/inputSearch/InputSearch.tsx
@@ -1,21 +1,45 @@
 import { Input } from 'antd';
-import { ChangeEventHandler } from 'react';
+import { ChangeEventHandler, FocusEventHandler, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import s from './input.module.scss';
 
 type Props = {
+    setSearchValue: (value: string) => void;
+    onSearch: () => void;
     onChange: ChangeEventHandler<HTMLInputElement>;
+    onFocus: FocusEventHandler<HTMLInputElement>;
+    onBlur: FocusEventHandler<HTMLInputElement>;
+    value: string;
 };
 
 const { Search } = Input;
 
-const InputSearch = ({ onChange }: Props) => {
+const InputSearch = ({
+    setSearchValue,
+    onSearch,
+    onChange,
+    onFocus,
+    onBlur,
+    value,
+}: Props) => {
+    const location = useLocation();
+    const searchParams = new URLSearchParams(location.search);
+    const queryUrl = searchParams.get('query') || '';
+
+    useEffect(() => {
+        setSearchValue(queryUrl);
+    }, [queryUrl, setSearchValue]);
+
     return (
         <Search
+            value={value}
             onChange={onChange}
-            allowClear
+            onFocus={onFocus}
+            onBlur={onBlur}
             enterButton="Search"
             placeholder="Find your event"
             className={s.input}
+            onSearch={onSearch}
         />
     );
 };
