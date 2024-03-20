713d3c1fdc8788aca87636b5399c486c6c6f04a6
Yury Kinchin <yury.kinchyn@gmail.com>
2024-03-20 19:49:39 +0300

    feat: add history reducers

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,16 +1,21 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { eventApi } from './eventsSlice/eventApi';
 import { favoriteApi } from './favoriteSlice/favoriteApi';
+import { historyApi } from './historySlice/historyApi';
+import { historyReducer } from './historySlice/historySlice';
 
 export const store = configureStore({
     reducer: combineReducers({
         [eventApi.reducerPath]: eventApi.reducer,
         [favoriteApi.reducerPath]: favoriteApi.reducer,
+        [historyApi.reducerPath]: historyApi.reducer,
+        history: historyReducer,
     }),
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware()
             .concat(eventApi.middleware)
-            .concat(favoriteApi.middleware),
+            .concat(favoriteApi.middleware)
+            .concat(historyApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
