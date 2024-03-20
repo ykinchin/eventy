49f4d01769737350fd4f1847794b57c0985312b5
Yury Kinchin <yury.kinchyn@gmail.com>
2024-03-20 20:04:58 +0300

    feat: delete filters

--- src/pages/events-page/EventsPage.tsx
+++ src/pages/events-page/EventsPage.tsx
@@ -1,24 +1,21 @@
+import { Empty, Flex, Pagination, Typography } from 'antd';
 import { useState } from 'react';
 import EventList from '../../components/shared/eventList/EventList';
-import useDebounce from '../../hooks/useDebounce';
-import { Sort } from '../../shared/types/eventTypes';
-import { useGetEventsQuery } from '../../store/eventsSlice/eventApi';
+import Loader from '../../components/shared/loader/Loader';
+import { useGetAllEventsQuery } from '../../store/eventsSlice/eventApi';
+
+const { Title } = Typography;
 
 const EventsPage = () => {
     const [currentPage, setCurrentPage] = useState(1);
-    const [searchValue, setSearchValue] = useState<null | string>(null);
-    const [cityValue, setCityValue] = useState<null | string>(null);
-    const [sortValue, setSortValue] = useState<Sort>('relevance,desc');
-    const debouncedSearch = useDebounce(searchValue, 300);
-    const debouncedCity = useDebounce(cityValue, 300);
 
-    const { data: { events, totalElements } = {}, isLoading } =
-        useGetEventsQuery({
-            page: currentPage,
-            searchValue: debouncedSearch,
-            city: debouncedCity,
-            sort: sortValue,
-        });
+    const {
+        data: { events, totalElements } = {},
+        isLoading,
+        isError,
+    } = useGetAllEventsQuery({
+        page: currentPage,
+    });
 
     const pagesNumber =
         totalElements && totalElements > 990
@@ -29,33 +26,36 @@ const EventsPage = () => {
         setCurrentPage(page);
     };
 
-    const handleSort = (value: Sort) => {
-        setSortValue(value);
-    };
-
-    const handleCitySearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setCityValue(e.currentTarget.value);
-        setCurrentPage(1);
-    };
-
-    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setSearchValue(e.currentTarget.value);
-        setCurrentPage(1);
-    };
-
     return (
-        <div style={{ height: '100%' }}>
-            <EventList
-                events={events}
-                pagesNumber={pagesNumber}
-                currentPage={currentPage}
-                isLoading={isLoading}
-                handleInputChange={handleInputChange}
-                handleSort={handleSort}
-                handlePageChange={handlePageChange}
-                handleCitySearch={handleCitySearch}
-            />
-        </div>
+        <>
+            {isLoading ? (
+                <Loader />
+            ) : (
+                <Flex
+                    vertical
+                    justify="space-between"
+                    style={{ height: '100%' }}
+                >
+                    <Flex vertical gap={40}>
+                        <Title level={3}>Explore world events</Title>
+                        {isError ? (
+                            <Empty />
+                        ) : (
+                            <EventList isLoading={isLoading} events={events} />
+                        )}
+                    </Flex>
+                    <Flex justify="center">
+                        <Pagination
+                            showSizeChanger={false}
+                            hideOnSinglePage={true}
+                            total={pagesNumber}
+                            current={currentPage}
+                            onChange={handlePageChange}
+                        />
+                    </Flex>
+                </Flex>
+            )}
+        </>
     );
 };
 
