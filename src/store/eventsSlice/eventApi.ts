173fe7f957edbdfd34370f0ccc82d8c39902147c
Yury Kinchin <yury.kinchyn@gmail.com>
2024-03-20 20:05:22 +0300

    refactor: move transforms to apiHelper

--- src/store/eventsSlice/eventApi.ts
+++ src/store/eventsSlice/eventApi.ts
@@ -2,70 +2,85 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import moment from 'moment';
 import { BASE_URL } from '../../shared/constants/url';
 import {
+    transformAllEvents,
+    transformFilteredEvents,
+    transformSingleEvents,
+} from '../../shared/helpers/apiHelpers';
+import {
+    FilteredEvent,
     PartialEvent,
-    ResponseType,
-    Sort,
+    SingleEventResponse,
 } from '../../shared/types/eventTypes';
 
-type eventsResponse = {
+type AllEventsResponse = {
     events: PartialEvent[];
     totalElements: number;
 };
 
-type QueryParams = {
-    eventIds?: string[] | null;
-    page?: number;
-    searchValue?: string | null;
-    sort?: Sort;
-    city?: string | null;
+type FilteredEventsResponse = {
+    events: FilteredEvent[];
 };
 
-const transformEvents = (response: ResponseType): eventsResponse => ({
-    events: response._embedded.events.map((event) => ({
-        name: event.name,
-        id: event.id,
-        city: event?._embedded?.venues?.[0]?.city?.name || 'Unknown City',
-        country:
-            event?._embedded?.venues?.[0]?.country?.name || 'Unknown Country',
-        genre:
-            event.classifications?.map(
-                (classification) => classification.segment.name,
-            ) || [],
-        date: event.dates.start.localDate,
-    })),
-    totalElements: response.page.totalElements,
-});
+type AllEventsParams = {
+    searchValue?: string | null;
+    eventIds?: string[] | null;
+    page?: number;
+};
+
+type EventId = string | undefined;
+
+type FilteredEventsParams = {
+    searchValue?: string | null;
+};
 
 export const eventApi = createApi({
     reducerPath: 'events',
     baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
     endpoints: (builder) => ({
-        getEvents: builder.query<eventsResponse, QueryParams>({
-            query: ({ page, searchValue, sort, city, eventIds }) => {
+        getAllEvents: builder.query<AllEventsResponse, AllEventsParams>({
+            query: ({ page, eventIds, searchValue }) => {
                 const formattedDate = moment()
                     .utc()
                     .format('YYYY-MM-DDTHH:mm:ss[Z]');
                 let queryString = `events.json?apikey=${import.meta.env.VITE_API_KEY}&size=${page ? '10' : '200'}&startDateTime=${formattedDate}`;
+                if (searchValue) {
+                    queryString += `&keyword=${searchValue}`;
+                }
                 if (eventIds && eventIds.length > 0) {
                     queryString += `&id=${eventIds.join(',')}`;
                 }
                 if (page) {
                     queryString += `&page=${page}`;
                 }
-                if (searchValue) {
-                    queryString += `&keyword=${searchValue}`;
-                }
-                if (sort) {
-                    queryString += `&sort=${sort}`;
-                }
-                if (city && city !== '') {
-                    queryString += `&city=${city}`;
-                }
                 return queryString;
             },
-            transformResponse: transformEvents,
+            transformResponse: transformAllEvents,
+        }),
+        getSingleEvent: builder.query<Partial<SingleEventResponse>, EventId>({
+            query: (eventId) => {
+                const queryString = `events.json?apikey=${import.meta.env.VITE_API_KEY}&id=${eventId}`;
+                return queryString;
+            },
+            transformResponse: transformSingleEvents,
+        }),
+        getFilteredEvents: builder.query<
+            FilteredEventsResponse,
+            FilteredEventsParams
+        >({
+            query: ({ searchValue }) => {
+                const formattedDate = moment()
+                    .utc()
+                    .format('YYYY-MM-DDTHH:mm:ss[Z]');
+                const queryString = `events.json?apikey=${import.meta.env.VITE_API_KEY}&size=5&keyword=${searchValue}&startDateTime=${formattedDate}`;
+                return queryString;
+            },
+            transformResponse: transformFilteredEvents,
         }),
     }),
 });
 
-export const { useGetEventsQuery } = eventApi;
+export const {
+    useGetAllEventsQuery,
+    useGetFilteredEventsQuery,
+    useGetSingleEventQuery,
+} = eventApi;
