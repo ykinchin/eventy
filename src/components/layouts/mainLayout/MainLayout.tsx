2af18d2a1ca1654e7f6272391a6aefacaa43bd7b
Yury Kinchin <yury.kinchyn@gmail.com>
2024-03-20 19:57:32 +0300

    feat: add loader

--- src/components/layouts/mainLayout/MainLayout.tsx
+++ src/components/layouts/mainLayout/MainLayout.tsx
@@ -1,10 +1,18 @@
 import { Layout } from 'antd';
 import { Content } from 'antd/es/layout/layout';
 import { Outlet } from 'react-router-dom';
+import { useAuth } from '../../../hooks/useAuth';
+import Loader from '../../shared/loader/Loader';
 import MainHeader from './header/MainHeader';
 import s from './mainLayout.module.scss';
 
 const MainLayout = () => {
+    const { isLoading } = useAuth();
+
+    if (isLoading) {
+        return <Loader />;
+    }
+
     return (
         <Layout style={{ height: '100%' }}>
             <MainHeader />
