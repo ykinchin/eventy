a232cfbfbf070ea91805dc4ace73c0a53ebbd8b5
Yury Kinchin <yury.kinchyn@gmail.com>
2024-03-20 20:01:08 +0300

    feat: create search page

---
+++ src/pages/search-page/SearchPage.tsx
@@ -0,0 +1,44 @@
+import { Empty, Flex, Typography } from 'antd';
+import { useLocation } from 'react-router-dom';
+import EventList from '../../components/shared/eventList/EventList';
+import Loader from '../../components/shared/loader/Loader';
+import { useGetAllEventsQuery } from '../../store/eventsSlice/eventApi';
+
+const { Title } = Typography;
+
+const SearchPage = () => {
+    const location = useLocation();
+    const searchParams = new URLSearchParams(location.search);
+    const searchValue = searchParams.get('query');
+
+    const {
+        data: { events } = {},
+        isLoading,
+        isError,
+    } = useGetAllEventsQuery({
+        searchValue,
+    });
+
+    return (
+        <>
+            {isLoading ? (
+                <Loader />
+            ) : (
+                <>
+                    {isError && events?.length === 0 ? (
+                        <Empty />
+                    ) : (
+                        <Flex vertical gap={40}>
+                            <Title level={4}>
+                                Search Results for "{searchValue}"
+                            </Title>
+                            <EventList events={events} />
+                        </Flex>
+                    )}
+                </>
+            )}
+        </>
+    );
+};
+
+export default SearchPage;
