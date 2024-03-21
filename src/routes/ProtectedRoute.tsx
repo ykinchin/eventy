1c43b5f7e6f62b0b5d5ffe29aef655e7971fc897
Yury Kinchin <yury.kinchyn@gmail.com>
2024-03-21 13:00:10 +0300

    feat: add private route wrapper

---
+++ src/routes/ProtectedRoute.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { useAuth } from '../hooks/useAuth';
+import { PATHS } from '../shared/constants/paths';
+
+interface Props {
+    children: React.ReactNode;
+}
+
+const ProtectedRoute = ({ children }: Props) => {
+    const { currentUser, isLoading } = useAuth();
+
+    if (!currentUser && isLoading) {
+        return <Navigate to={PATHS.main} replace />;
+    }
+
+    return <>{children}</>;
+};
+
+export { ProtectedRoute };
